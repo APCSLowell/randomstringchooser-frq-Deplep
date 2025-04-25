d5a3ba780915b07a37c24b5daf62b60b4794c6a2
Deplep <131609777+Deplep@users.noreply.github.com>
2025-04-24 22:00:03 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -2,16 +2,15 @@ import java.util.*;
 public class RandomStringChooser
 {
   private ArrayList<String> list;
-  private int temp;
   public RandomStringChooser(String [] a){
     list = new ArrayList<String>();
     for(int i = 0; i < a.length; i++){
-      list.get(i) = a[i];
+      list.add(a[i]);
     }
   }
   public String getNext(){
     if(list.size() > 0){
-      temp = ((int)Math.random()*list.size());
+      int temp = (int)(Math.random()*list.size());
       return list.remove(temp);
     } else {
       return "NONE";
